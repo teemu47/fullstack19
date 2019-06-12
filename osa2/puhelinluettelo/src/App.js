b773b13b9d28e34d9fa8127548ea76da1f62a180
Teemu Ijäs <ijasteemu@gmail.com>
2019-06-12 19:14:35 +0300

    2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, filterList}) =>
   <div>
@@ -34,16 +35,19 @@ const Person = ({person}) =>
   <p>{`${person.name}: ${person.number}`}</p>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
   const personsToShow = persons.filter(person => person.name.toLowerCase().indexOf(filter) >= 0)
+  
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, [])
   
   const addPerson = (event) => {
     event.preventDefault()
