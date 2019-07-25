190fdc2f66bc391a63e59f96d8a20ccd68475ff4
Teemu Ijäs <ijasteemu@gmail.com>
2019-07-25 13:05:24 +0300

    4.2

---
+++ osa4/bloglist/app.js
@@ -0,0 +1,17 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
+
