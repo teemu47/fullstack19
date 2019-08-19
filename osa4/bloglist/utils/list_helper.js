5269c447fd07527c0cf57c573fdb5b9148e19705
Teemu Ijäs <ijasteemu@gmail.com>
2019-08-20 00:41:02 +0300

    refactor

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -3,38 +3,23 @@ const dummy = (blogs) => 1
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum += blog.likes, 0)
 
 const favouriteBlog = (blogs) => {
-  const favouriteBlog = blogs.reduce((favourite, blog) => {
-    if ((Object.keys(favourite).length === 0 && favourite.constructor === Object) || blog.likes > favourite.likes) {
-      favourite.title = blog.title
-      favourite.author = blog.author
-      favourite.likes = blog.likes
-    }
-    return favourite
-  }, {})
-  return favouriteBlog
+  const result = blogs.reduce((prev, next) => prev.likes > next.likes ? prev : next, {})
+  return (({title, author, likes}) => ({title, author, likes})) (result)
+  // return {title: result.title, author: result.author, likes: result.likes}
 }
 
 const mostBlogs = (blogs) => {
   const authors = blogs.reduce((list, blog) => {
     let author = list.find((entry) => entry.author === blog.author)
     if (!author) {
-      author = {
-        author: blog.author,
-        blogs: 0
-      }
+      author = { author: blog.author, blogs: 0 }
       list.push(author)
     }
-    author.blogs += 1
+    author.blogs++
     return list
   }, [])
   
-  const authorWithMostBlogs = authors.reduce((mostBlogs, author) => {
-    if ((Object.keys(mostBlogs).length === 0 && mostBlogs.constructor === Object) || author.blogs > mostBlogs.blogs) {
-      mostBlogs = author
-    }
-    return mostBlogs
-  }, {})
-  
+  const authorWithMostBlogs = authors.reduce((prev, next) => prev.blogs > next.blogs ? prev : next, {})
   return authorWithMostBlogs
 }
 
