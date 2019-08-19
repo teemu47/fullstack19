58c574c98c54d6eba9f53c97644c566eacfaca0e
Teemu Ijäs <ijasteemu@gmail.com>
2019-08-19 22:31:16 +0300

    4.5

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -1,8 +1,21 @@
+const dummy = (blogs) => 1
 
-const dummy = (blogs) => {
-  return 1
+const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum += blog.likes, 0)
+
+const favouriteBlog = (blogs) => {
+  const favouriteBlog = blogs.reduce((favourite, blog) => {
+    if ((Object.keys(favourite).length === 0 && favourite.constructor === Object) || blog.likes > favourite.likes) {
+      favourite.title = blog.title
+      favourite.author = blog.author
+      favourite.likes = blog.likes
+    }
+    return favourite
+  }, {})
+  return favouriteBlog
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favouriteBlog
 }
