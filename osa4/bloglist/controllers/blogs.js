a0fcc16c1ab6a9179d7e7c25e48eb144b0f02710
Teemu Ijäs <ijasteemu@gmail.com>
2019-09-04 18:51:15 +0300

    4.8

--- osa4/bloglist/controllers/blogs.js
+++ osa4/bloglist/controllers/blogs.js
@@ -1,12 +1,9 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(x => x.toJSON()))
 })
 
 blogRouter.post('/', (request, response) => {
