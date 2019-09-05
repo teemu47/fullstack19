8af6b4c6cf38937a0a195c7384ef3bc9f7191e2e
Teemu Ijäs <ijasteemu@gmail.com>
2019-09-05 11:11:21 +0300

    4.15

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.errorHandler)
 
---
+++ osa4/bloglist/controllers/users.js
@@ -0,0 +1,31 @@
+const userRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+userRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users.map(u => u.toJSON()))
+})
+
+userRouter.post('/', async(request, response, next) => {
+  try {
+    const body = request.body
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  
+    const user = new User({
+      name: body.name,
+      username: body.username,
+      passwordHash: passwordHash
+    })
+  
+    const newUser = await user.save()
+    response.json(newUser.toJSON())
+  } catch (e) {
+    next(e)
+  }
+  
+})
+
+
+module.exports = userRouter
