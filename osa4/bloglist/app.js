c7512f6691e6b42b00e7899e950d7aef4f91455a
Teemu Ijäs <ijasteemu@gmail.com>
2019-09-05 10:30:16 +0300

    adding middleware

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
@@ -12,6 +13,8 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.errorHandler)
 
 module.exports = app
 
--- osa4/bloglist/controllers/blogs.js
+++ osa4/bloglist/controllers/blogs.js
@@ -2,16 +2,11 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogRouter.get('/', async (request, response) => {
-  try {
     const blogs = await Blog.find({})
     response.json(blogs.map(x => x.toJSON()))
-  } catch (e) {
-    console.error(e.message)
-    response.status(500).end()
-  }
 })
 
-blogRouter.post('/', async (request, response) => {
+blogRouter.post('/', async (request, response, next) => {
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
@@ -23,25 +18,20 @@ blogRouter.post('/', async (request, response) => {
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog.toJSON)
   } catch (e) {
-    console.error('error message', e.message)
-    if (e.name === 'ValidationError') {
-      response.status(400).json({error: e.message})
-    }
-    response.status(500).end()
+    next(e)
   }
 })
 
-blogRouter.delete('/:id', async (request, response) => {
+blogRouter.delete('/:id', async (request, response, next) => {
   try {
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
   } catch (e) {
-    console.error('error message', e.message)
-    response.status(500).end()
+    next(e)
   }
 })
 
-blogRouter.put('/:id', async (request, response) => {
+blogRouter.put('/:id', async (request, response, next) => {
   const blog = {
     likes: request.body.likes
   }
@@ -50,8 +40,7 @@ blogRouter.put('/:id', async (request, response) => {
     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
     response.json(updatedBlog.toJSON)
   } catch (e) {
-    console.error('error message', e.message)
-    response.status(500).end()
+    next(e)
   }
 })
 
