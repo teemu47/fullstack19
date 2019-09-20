cfc38e29df03432976daa27ad3feda4dabd592cb
Teemu Ijäs <ijasteemu@gmail.com>
2019-09-20 21:54:13 +0300

    6.21

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -8,7 +8,7 @@ import Notification from './components/Notification'
 const App = (props) => {
   useEffect(() => {
     props.initializeAnecdotes();
-  }, [])
+  }, [props])
   
   return (
     <div>
