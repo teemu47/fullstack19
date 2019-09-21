397d63546cf52a9dc1a0d0bfc97c5b3fed7bcc30
Teemu Ijäs <ijasteemu@gmail.com>
2019-09-21 15:19:03 +0300

    return user info when updating blog

--- osa7/bloglist/bloglist-backend/controllers/blogs.js
+++ osa7/bloglist/bloglist-backend/controllers/blogs.js
@@ -74,7 +74,7 @@ blogRouter.put('/:id', async (request, response, next) => {
   }
   
   try {
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true}).populate('user', {username: 1, name: 1})
     response.json(updatedBlog.toJSON())
   } catch (e) {
     next(e)
